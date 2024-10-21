6023ade3e0c5a4150cf0b4000272d841d8485fcb
Harsha <iiharsha10@gmail.com>
2024-10-21 15:33:44 +0000

    change remap.lua

--- lua/theharsha/remap.lua
+++ lua/theharsha/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n" , "<leader>fe", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
 
 -- for shifting the selected lines up or down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -14,10 +14,10 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- clipboard remaps
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("i", "jj", "<Esc>")
 
@@ -33,4 +33,5 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- command line write smoothening
-vim.keymap.set("n" , ";", ":")
+vim.keymap.set("n", ";", ":", { noremap = true, silent = false })
+vim.keymap.set("c", "W", "w", { noremap = true, silent = false })
